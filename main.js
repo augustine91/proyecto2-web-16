7eb618489bb4d6083790764a06e2337cdfb1b19e
augustine91 <aguztin20@gmail.com>
2016-11-23 10:24:12 -0600

    mejoras
    nuevos metodos

--- main.js
+++ main.js
@@ -1,36 +1,49 @@
+var contador = 0;
+var animales = [];
+$('tbody').sortable();
+var Persister = {
+    save: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    load: function(key, default_value) {
+        return localStorage.getItem(key) || default_value;
+    },
+    saveObj: function(key, value) {
+        var json_string = JSON.stringify(value);
+        this.save(key, json_string);    
+    },
+    loadObj: function(key, default_value) {
+        var json_string = this.load(key, default_value);
+        return JSON.parse(json_string);
+    }
+};
+
+function load_data() {
+    contador = Persister.load('clicks', 1);
+    animales = Persister.loadObj('animales', "[]");
+    $('#contador').val(contador);
+
+    $('#animales').html('');
+    for (var i = 0; i < animales.length; i++) {
+        $('#animales').append('<li>' + animales[i].nombre + '</li>');
+    }
+}
+
 $(document).ready(function() {
-    //Helper function to keep table row from collapsing when being sorted
-    var fixHelperModified = function(e, tr) {
-        var $originals = tr.children();
-        var $helper = tr.clone();
-        $helper.children().each(function(index)
-        {
-          $(this).width($originals.eq(index).width())
-        });
-        return $helper;
-    };
-
-    //Make diagnosis table sortable
-    $("#diagnosis_list tbody").sortable({
-        helper: fixHelperModified,
-        stop: function(event,ui) {renumber_table('#diagnosis_list')}
-    }).disableSelection();
-
-    //Delete button in table rows
-    $('table').on('click','.btn-delete',function() {
-        tableID = '#' + $(this).closest('table').attr('id');
-        r = confirm('Delete this item?');
-        if(r) {
-            $(this).closest('tr').remove();
-            renumber_table(tableID);
-            }
+    load_data();
+    $('#incrementar').click(function() {
+        contador++;
+        Persister.save('clicks', contador);
+        $('#contador').val(contador);
+    });
+    $('#save').click(function(event) {
+        var animal = $('#animal').val();
+        animales.push({nombre: animal});
+        Persister.saveObj('animales', animales);
+        $('#animales').append('<li>' + animal + '</li>');
+    });
+    $('#clear').click(function(event) {
+        localStorage.clear();
+        load_data();
     });
 });
-
-//Renumber table rows
-function renumber_table(tableID) {
-    $(tableID + " tr").each(function() {
-        count = $(this).parent().children().index($(this)) + 1;
-        $(this).find('.priority').html(count);
-    });
-}
