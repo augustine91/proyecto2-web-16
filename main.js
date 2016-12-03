92a8ef08522f83a964364fcbfa9b50e04c263ce6
augustine91 <aguztin20@gmail.com>
2016-12-03 16:21:39 -0600

    Chances
    javascript and html created links pages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var contador = 0;
 var animales = [];
-$('tbody').sortable();
+
 var Persister = {
     save: function (key, value) {
         localStorage.setItem(key, value);
@@ -10,7 +10,7 @@ var Persister = {
     },
     saveObj: function(key, value) {
         var json_string = JSON.stringify(value);
-        this.save(key, json_string);    
+        this.save(key, json_string);
     },
     loadObj: function(key, default_value) {
         var json_string = this.load(key, default_value);
@@ -28,9 +28,23 @@ function load_data() {
         $('#animales').append('<li>' + animales[i].nombre + '</li>');
     }
 }
-
 $(document).ready(function() {
-    load_data();
+    //mostramos por defecto la primera página
+    $('#page_1').show();
+    $('.navbar-nav a').click(function(event) {
+        event.preventDefault();
+        //ocultamos todas las secciones
+        $('.starter-template').hide();
+        //quitamos todas las clases active de todos los elementos li
+        $('li').removeClass('active');
+        //agregamos la clase active al li padre
+        $(this).parent().addClass('active');
+        //capturamos el valor del href del elemento cliqueado
+        var current_page = $(this).attr('href');
+        //mostramos la página con el id seleccionado
+        $(current_page).show();
+    });
+     load_data();
     $('#incrementar').click(function() {
         contador++;
         Persister.save('clicks', contador);
